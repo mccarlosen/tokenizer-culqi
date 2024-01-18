41553a13a69f61725effa447dd38a63545930888
Carlos Meneses <osen.10112@gmail.com>
2024-01-18 02:43:50 -0500

    Delete index.js

--- .build/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const createToken_1 = __importDefault(require("./functions/createToken"));
-module.exports.createToken = createToken_1.default;
-//# sourceMappingURL=index.js.map
