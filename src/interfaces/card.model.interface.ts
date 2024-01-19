163f363419009e84ac337f288ece2725547a84ea
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:25 -0500

    Update card.model.interface.ts

--- src/interfaces/card.model.interface.ts
+++ src/interfaces/card.model.interface.ts
@@ -1,6 +1,6 @@
-import { CardInterface } from "./card.interface";
+import { CardInterface } from './card.interface'
 
 export interface CardModelInterface {
-	save(card: CardInterface): Promise<CardInterface>;
-	find(id: string): Promise<CardInterface | null>
+  save: (card: CardInterface) => Promise<CardInterface>
+  find: (id: string) => Promise<CardInterface | null>
 }
