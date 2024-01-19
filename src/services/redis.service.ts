db72f074bb573569473c909e0d86df5dbdae678c
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:34 -0500

    Update redis.service.ts

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,18 +1,17 @@
-import { createClient, RedisClientType } from "redis";
-import RedisConfig from "../config/redis.config";
+import { createClient, RedisClientType } from 'redis'
 export class RedisService {
-  private static instance: RedisService;
-  private client: RedisClientType;
+  private static readonly instance: RedisService
+  private readonly client: RedisClientType
 
-  constructor() {
-    this.client = createClient();
+  constructor () {
+    this.client = createClient()
   }
 
-  async connect(): Promise<RedisClientType> {
+  async connect (): Promise<RedisClientType> {
     return await this.client.connect()
   }
 
-  getClient(): RedisClientType {
-    return this.client;
+  getClient (): RedisClientType {
+    return this.client
   }
 }
