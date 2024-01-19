1a4496bcdcc8696d28386ebecba3435d83704910
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:28 -0500

    Update use-authorization-token-validate.lib.ts

--- src/libs/use-authorization-token-validate.lib.ts
+++ src/libs/use-authorization-token-validate.lib.ts
@@ -1,10 +1,17 @@
-export const useAuthorizationTokenValidate = () => {
-	const check = (token: string): boolean => {
-		const regex = /^pk_test_[a-zA-Z0-9]{20}$/;
-		return regex.test(token);
-	}
+interface AuthorizationTokenValidator {
+  check: (tokenValue: string | undefined) => boolean
+}
+export const useAuthorizationTokenValidate = (): AuthorizationTokenValidator => {
+  const check = (tokenValue: string | undefined): boolean => {
+    if (tokenValue === null || tokenValue === undefined) {
+      return false
+    }
+    const [, bearerToken] = tokenValue.split(' ')
+    const regex = /^pk_test_[a-zA-Z0-9]{16}$/
+    return regex.test(bearerToken)
+  }
 
-	return {
-		check
-	}
+  return {
+    check
+  }
 }
