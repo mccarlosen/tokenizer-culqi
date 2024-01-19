22e980798e1b0b1a60e30f35c85d424505c055fb
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:31 -0500

    Update use-validator.lib.ts

--- src/libs/use-validator.lib.ts
+++ src/libs/use-validator.lib.ts
@@ -1,37 +1,36 @@
-import { luhnVerify } from "./luhn-verify.lib";
+import { luhnVerify } from './luhn-verify.lib'
 
-const validateRule = (value: any, rule: string, params: Array<string>) => {
-	switch (rule) {
-		case 'required':
-			return value !== undefined && value !== null && value !== '';
-		case 'numeric':
-			return typeof value === 'number' && !isNaN(value);
-		case 'string':
-			return typeof value === 'string';
-		case 'min':
-			return String(value).length >= parseInt(params[0], 10);
-		case 'max':
-			return String(value).length <= parseInt(params[0], 10);
-		case 'luhnFormat':
-			return luhnVerify(value);
-		default:
-			return true;
-	}
+const validateRule = (value: any, rule: string, params: string[]): boolean => {
+  switch (rule) {
+    case 'required':
+      return value !== undefined && value !== null && value !== ''
+    case 'numeric':
+      return typeof value === 'number' && !isNaN(value)
+    case 'string':
+      return typeof value === 'string'
+    case 'min':
+      return String(value).length >= parseInt(params[0], 10)
+    case 'max':
+      return String(value).length <= parseInt(params[0], 10)
+    case 'luhnFormat':
+      return luhnVerify(value)
+    default:
+      return true
+  }
 }
 
-export const validate = (data: Array<any>, rules: object) => {
-	for (let field in rules) {
-	  if (rules.hasOwnProperty(field)) {
-		const fieldRules = rules[field].split('|');
-		for (let rule of fieldRules) {
-		  const [ruleName, ...params] = rule.split(':');
-		  const isValid = validateRule(data[field], ruleName, params);
-		  if (!isValid) {
-			return `${field} does not meet the rule: ${rule}`;
-		  }
-		}
-	  }
-	}
-	return true;
+export const useValidate = (data: any[], rules: object): boolean | string => {
+  for (const field in rules) {
+    if (Object.prototype.hasOwnProperty.call(rules, field) === true) {
+      const fieldRules: string[] = rules[field].split('|')
+      for (const rule of fieldRules) {
+        const [ruleName, ...params] = rule.split(':')
+        const isValid = validateRule(data[field], ruleName, params)
+        if (!isValid) {
+          throw new Error(`${field} does not meet the rule: ${rule}`)
+        }
+      }
+    }
+  }
+  return true
 }
-  
