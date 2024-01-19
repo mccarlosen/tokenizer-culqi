d40899daeff4a7b2049df4f5487896deaae73c80
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:30 -0500

    Update use-headers.lib.ts

--- src/libs/use-headers.lib.ts
+++ src/libs/use-headers.lib.ts
@@ -1,3 +1,5 @@
-export const useHeaderValidate = (headers: object, matchHeader: string) => {
-	return headers && headers[matchHeader.toLowerCase()]
+import { APIGatewayProxyEventHeaders } from 'aws-lambda'
+
+export const useHeaderValidate = (headers: APIGatewayProxyEventHeaders, matchHeader: string): string | undefined => {
+  return headers[matchHeader.toLowerCase()]
 }
