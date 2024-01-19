298df7802283fd28b0af58ed048aeb3c8b871620
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:10:53 -0500

    Delete index.js

--- .build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenizerGetCardDataHandler = exports.tokenizerCreateTokenHandler = void 0;
-var handler_1 = require("./src/functions/tokenizer/handler");
-Object.defineProperty(exports, "tokenizerCreateTokenHandler", { enumerable: true, get: function () { return handler_1.tokenizerCreateTokenHandler; } });
-Object.defineProperty(exports, "tokenizerGetCardDataHandler", { enumerable: true, get: function () { return handler_1.tokenizerGetCardDataHandler; } });
-//# sourceMappingURL=index.js.map
