5b97d503940151ca2163b788b3b30613483c3a89
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 07:44:16 -0500

    Create card.model.interface.ts

---
+++ src/interfaces/card.model.interface.ts
@@ -0,0 +1,6 @@
+import { CardInterface } from "./card.interface";
+
+export interface CardModelInterface {
+	save(card: CardInterface): Promise<CardInterface>;
+	find(id: string): Promise<CardInterface | null>
+}
