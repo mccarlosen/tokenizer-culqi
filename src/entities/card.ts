75691d272ec0cf8379a16f123455875db2ccba34
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 07:44:15 -0500

    Create card.ts

---
+++ src/entities/card.ts
@@ -0,0 +1,10 @@
+import { CardInterface } from "../interfaces/card.interface";
+
+export class Card implements CardInterface {
+	cardNumber: number;
+	cvv: number;
+	expirationMonth: string;
+	expirationYear: string;
+	email: string;
+	token: string;
+}
