6b5c625d9a062de058acbc96b46efe632206ef01
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:28 -0500

    Update use-card-token-validate.lib.ts

--- src/libs/use-card-token-validate.lib.ts
+++ src/libs/use-card-token-validate.lib.ts
@@ -1,17 +1,20 @@
-import TokenizerConfig from "../config/tokenizer.config"
+import TokenizerConfig from '../config/tokenizer.config'
+interface CardTokenValidator {
+  isValid: (token: string) => boolean
+}
 
-export const useCardTokenValidate = () => {
-	const isValid = (token: string | number): boolean => {
-		if (!(String(token).length == TokenizerConfig.TOKENIZER_TOKEN_LENGTH)) {
-			return false
-		}
-		if (!(/[a-zA-Z]/.test(String(token)) && /\d/.test(String(token)))) {
-			return false
-		}
-		return true
-	}
+export const useCardTokenValidate = (): CardTokenValidator => {
+  const isValid = (token: string): boolean => {
+    if (!(String(token).length === TokenizerConfig.TOKENIZER_TOKEN_LENGTH)) {
+      return false
+    }
+    if (!(/[a-zA-Z]/.test(String(token)) && /\d/.test(String(token)))) {
+      return false
+    }
+    return true
+  }
 
-	return {
-		isValid
-	}
+  return {
+    isValid
+  }
 }
