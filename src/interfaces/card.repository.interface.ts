624306aa15d62e0ce665d2a66f61685ee845bcf6
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 07:44:17 -0500

    Create card.repository.interface.ts

---
+++ src/interfaces/card.repository.interface.ts
@@ -0,0 +1,6 @@
+import { CardInterface } from "./card.interface";
+
+export interface CardRepositoryInterface {
+	save(card: CardInterface): Promise<CardInterface>
+	getInfo(token: string): Promise<CardInterface | null>
+}
