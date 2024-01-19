7bcae106ef1202bacf774a4d041ab0571e0bda2c
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:29 -0500

    Update use-create-token.lib.ts

--- src/libs/use-create-token.lib.ts
+++ src/libs/use-create-token.lib.ts
@@ -1,9 +1,9 @@
-const crypto = require("crypto");
+import * as crypto from 'crypto'
 export const useCreateToken = (length: number): string => {
-	const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	const charactersLength = characters.length;
-  
-	return Array.from(crypto.randomBytes(length))
-	  .map((byte: any) => characters[byte % charactersLength])
-	  .join('');
-};
+  const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  const charactersLength = characters.length
+
+  return Array.from(crypto.randomBytes(length))
+    .map((byte: any) => characters[byte % charactersLength])
+    .join('')
+}
