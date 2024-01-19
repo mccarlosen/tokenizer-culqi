af159b09d14a246302392a9b9196e232e22fd627
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 15:11:27 -0500

    Update luhn-verify.lib.ts

--- src/libs/luhn-verify.lib.ts
+++ src/libs/luhn-verify.lib.ts
@@ -1,11 +1,11 @@
-export const luhnVerify = (cardNumber: number) => {
-    const digits: number[] = Array.from(String(cardNumber), Number);
-    for (let i = digits.length - 2; i >= 0; i -= 2) {
-        digits[i] *= 2;
-        if (digits[i] > 9) {
-            digits[i] -= 9;
-        }
+export const luhnVerify = (cardNumber: number): boolean => {
+  const digits: number[] = Array.from(String(cardNumber), Number)
+  for (let i = digits.length - 2; i >= 0; i -= 2) {
+    digits[i] *= 2
+    if (digits[i] > 9) {
+      digits[i] -= 9
     }
-    const summationTotal = digits.reduce((acc, digit) => acc + digit, 0);
-    return summationTotal % 10 === 0;
+  }
+  const summationTotal = digits.reduce((acc, digit) => acc + digit, 0)
+  return summationTotal % 10 === 0
 }
