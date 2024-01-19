b718dab83e1dc0c8eb7a1c7c55cfd91bc19f0090
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 07:44:14 -0500

    Create tokenizer.config.ts

---
+++ src/config/tokenizer.config.ts
@@ -0,0 +1,6 @@
+const TokenizerConfig = {
+	TOKENIZER_EXPIRATION_TOKEN: Number(process.env.TOKENIZER_EXPIRATION_TOKEN) || 0,
+	TOKENIZER_TOKEN_LENGTH: Number(process.env.TOKENIZER_TOKEN_LENGTH)
+}
+
+export default TokenizerConfig
