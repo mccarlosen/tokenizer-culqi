4f7e90f5c2827f88ec4365643e9c2400386445ee
Carlos Meneses <osen.10112@gmail.com>
2024-01-19 23:11:39 -0500

    Update redis.service.ts

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,10 +1,16 @@
 import { createClient, RedisClientType } from 'redis'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
 export class RedisService {
   private static readonly instance: RedisService
   private readonly client: RedisClientType
 
   constructor () {
-    this.client = createClient()
+    this.client = createClient({
+      url: `redis://${String(process.env.REDIS_HOST)}:${String(process.env.REDIS_PORT)}`
+    })
   }
 
   async connect (): Promise<RedisClientType> {
